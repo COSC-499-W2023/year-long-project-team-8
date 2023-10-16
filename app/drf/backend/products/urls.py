655a31208de338ad61a95e245f543ce6e7cff604
Mackenzie Kudrenecky <mkudrene@student.ubc.ca>
2023-10-15 23:21:08 -0700

    updated tests

--- app/drf/backend/products/urls.py
+++ app/drf/backend/products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.ProductDetailView.as_view())
+    path('<int:pk>/', views.ProductDetailView.as_view(), name='product-detail')
 ]
