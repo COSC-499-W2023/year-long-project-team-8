f1a75d4fc1115c9f95b23c9c29c8eab6b10f7f7e
aroizmand <alexander.roizmand@gmail.com>
2023-10-17 14:50:05 -0700

    Ceated homepage file and manage to reroute when use logs in and is given access

--- app/drf/front-end/App.js
+++ app/drf/front-end/App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { View, StatusBar } from "react-native";
 import Landing from "./components/landing/Landing.js";
+import HomePage from "./components/homePage/HomePage.js";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <>
-      <StatusBar barStyle="default" />
-      <View style={{ flex: 1 }}>
-        <Landing />
-      </View>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Landing"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Landing" component={Landing} />
+        <Stack.Screen name="HomePage" component={HomePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
