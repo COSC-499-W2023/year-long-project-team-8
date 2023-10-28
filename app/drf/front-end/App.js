b6aa0cf9a77e8ffaf791a57bcfbca1d44872704c
aroizmand <alexander.roizmand@gmail.com>
2023-10-27 18:20:58 -0700

    Page lands on Landing

--- app/drf/front-end/App.js
+++ app/drf/front-end/App.js
@@ -14,7 +14,7 @@ const App = () => {
       <StatusBar />
       <NavigationContainer>
         <Stack.Navigator
-          initialRouteName="Tabs"
+          initialRouteName="Landing"
           screenOptions={{
             headerShown: false,
           }}
