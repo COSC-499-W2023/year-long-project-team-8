316d97de3d13beee13d72db7d122a3679ad4cd9f
Brandon Mack <brandonjmack@protonmail.com>
2023-10-30 09:43:58 -0700

    completed profile page, minor tweaks neccesary still

--- app/drf/front-end/components/profilePage/profilePage.js
+++ app/drf/front-end/components/profilePage/profilePage.js
@@ -1,24 +1,87 @@
 import React from 'react';
-import {View, Text, Image, TouchableOpacity, SafeAreaView, StatusBar} from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
-
-
+import { View, Text, Image, SafeAreaView, TouchableOpacity, Dimensions } from 'react-native';
 import loginStyles from './profilePageStyles.js';
-import login from "../loginSignup/Login";
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const ProfilePage = () => {
   return (
+    <SafeAreaView style={loginStyles.safeArea}>
+      <View style={loginStyles.background}>
+        <View style={loginStyles.topHalf} />
+        <View style={loginStyles.bottomHalf} />
+        <View style={loginStyles.profilePictureContainer}>
+          <Text style={loginStyles.profileTag}> Profile </Text>
+          <View style={loginStyles.profileIconContainer}>
 
-            <SafeAreaView style={loginStyles.safeArea}>
-                <View style={loginStyles.topHalf}>
-                    <Text style={loginStyles.usernameText}> @username </Text>
-                    <View style={loginStyles.topBox}/>
-                    <View style={loginStyles.bottomHalf}/>
-                        <View style={loginStyles.circle}/>
-                        <Image style={loginStyles.profilePicture} source={require('../../assets/images/dummyPFP/pfp.png')} />
+            <View>
 
-                </View>
-            </SafeAreaView>
+                <Image
+                  style={[loginStyles.profileIcons, {top: 30}]}
+                  source={require('../../assets/images/profilePage/fork.png')}
+                />
+                <Image
+                    style={[loginStyles.profileIcons, {top: 42.5, right: 82.5}]}
+                    source={require('../../assets/images/profilePage/pizza.png')}
+                />
+                <Image
+                  style={[loginStyles.profileIcons, {top: 55, right: 165 }]}
+                  source={require('../../assets/images/profilePage/hamburger.png')}
+                />
+            </View>
+            <View>
+              <Image
+                style={[loginStyles.profileIcons, { left: 170, bottom: 90 }]}
+                source={require('../../assets/images/profilePage/chinese-food.png')}
+              />
+              <Image
+                style={[loginStyles.profileIcons, { left: 92.5, bottom: 77.5 }]}
+                source={require('../../assets/images/profilePage/kebab.png')}
+              />
+            </View>
+            </View>
+          <Image
+            style={loginStyles.profilePicture}
+            source={require('../../assets/images/profilePage/pfp.png')}
+          />
+        </View>
+        <View style={loginStyles.mainContainer}>
+          <Text style={loginStyles.userText}> Brandon Mack </Text>
+          <View style={loginStyles.locContainer}>
+            <Image
+              style={loginStyles.locPng}
+              source={require('../../assets/images/profilePage/location.png')}
+            />
+            <Text style={loginStyles.userLocation}> Kelowna, BC </Text>
+          </View>
+          <View style={loginStyles.contactInfoContainer}>
+            <View style={loginStyles.contactPngContainer}>
+              <Image style={loginStyles.emailPng} source={require('../../assets/images/profilePage/email.png')} />
+              <Image style={loginStyles.phonePng} source={require('../../assets/images/profilePage/telephone.png')} />
+            </View>
+            <View style={loginStyles.contactTextContainer}>
+              <Text style={loginStyles.email}> testuser@testuser.com </Text>
+              <Text style={loginStyles.phone}> +1 (111) 111 - 1111</Text>
+            </View>
+          </View>
+          <View style={loginStyles.postContainer}>
+            <View style={[loginStyles.post, {right: 28}]}>
+            <Text> sample post </Text>
+            </View>
+            <View style={loginStyles.post}>
+              <Text> sample post </Text>
+            </View>
+            <View style={[loginStyles.post, {left: 28}]}>
+              <Text> sample post </Text>
+            </View>
+          </View>
+            <TouchableOpacity style={loginStyles.button}>
+              <Text style={loginStyles.buttonText}> Edit Profile</Text>
+            </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
--- app/drf/front-end/components/profilePage/profilePageStyles.js
+++ app/drf/front-end/components/profilePage/profilePageStyles.js
@@ -1,7 +1,9 @@
 import { StyleSheet, Dimensions } from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
 
-const colors = {
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+export const colors = {
   lightOrange: '#F8B951',
   mediumOrange: '#FCA63C',
   darkOrange: '#DB6D2A',
@@ -11,68 +13,135 @@ const colors = {
 const loginStyles = StyleSheet.create({
   safeArea: {
     flex: 1,
+    height: windowHeight,
+  },
+  background: {
+    flex: 1,
+    flexDirection: 'column',
   },
   topHalf: {
+    flex: 2.5,
+    backgroundColor: colors.darkGray,
+  },
+  bottomHalf: {
+    flex: 7,
+    backgroundColor: colors.lightOrange,
+  },
+  profilePictureContainer: {
+    position: 'absolute',
+    width: windowWidth,
     alignItems: 'center',
   },
-   usernameText: {
-    fontSize: 40,
+  profileTag: {
+    fontSize: 22,
     fontWeight: 'bold',
-    color: 'white',
-  },
-
-
-
-
-  /*
-  circle: {
-    width: 150,
-    height: 150,
-    borderRadius: 75,
-    top: -180,
-    borderColor: 'white',
-    borderWidth: 2,
+    color: colors.lightOrange,
+    top: -5,
   },
   profilePicture: {
-    width: 146,
-    height: 146,
-    borderRadius: 73,
+    width: 140,
+    height: 140,
+    borderRadius: 70,
+    borderColor: 'white',
+    borderWidth: 3,
+    top: 85,
   },
-
-  */
-
-
-
-  userInformation: {},
-  horizontalLine: {
-    width: '100%',
-    height: 2,
-    backgroundColor: 'black',
+  profileIconContainer: {
+    position: 'absolute',
   },
-  text: {
-    fontSize: 25,
-    color: 'black',
-    left: 80,
-  },
-  button: {
-    backgroundColor: colors.lightOrange,
-    padding: 10,
-    borderRadius: 50,
-    width: 250,
-    height: 60,
-    bottom: 120,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    top: 7,
-  },
-  icons: {
+  profileIcons: {
     width: 40,
     height: 40,
+    opacity: 0.25,
   },
+  mainContainer: {
+    position: 'absolute',
+    top: '40%',
+    width: windowWidth,
+    alignItems: 'center',
+  },
+  userText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'black',
+    bottom: '6%',
+  },
+  locContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    bottom: 10,
+  },
+  userLocation: {
+    fontSize: 15,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  locPng: {
+    height: 15,
+    width: 15,
+  },
+  contactInfoContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+
+  },
+  contactPngContainer: {
+    top: 24,
+    right: '5%'
+  },
+  emailPng: {
+
+  },
+  phonePng: {
+    top: 18,
+  },
+  contactTextContainer: {
+    top: 20,
+    left: '15%'
+  },
+  email: {
+    fontSize: 20,
+    color: 'black',
+  },
+  phone: {
+    fontSize: 20,
+    color: 'black',
+    top: 25,
+  },
+  postContainer: {
+    height: 120,
+    width: windowWidth + 5,
+    top: 75,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderColor: 'black',
+    borderWidth: 1,
+
+  },
+  post: {
+    height: 90,
+    width: 90,
+    borderRadius: 100,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: 104,
+    height: 55,
+    width: 250,
+    borderRadius: 50,
+    backgroundColor: '#404040'
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+
 });
 
 export default loginStyles;
