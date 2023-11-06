8f6972a8d17babbb250e44b34d07e3a5ec892758
Joss White <jossw7@gmail.com>
2023-11-05 19:56:54 -0800

    Refactored user tests for simlpejwt format

--- app/drf/backend/tests/tests.py
+++ app/drf/backend/tests/tests.py
@@ -6,9 +6,7 @@ from users.models import User
 from django.test import TestCase
 from products.serializers import ProductSerializer
 from rest_framework.test import APIClient
-from users.permissions import UserPermission
-
-
+from rest_framework_simplejwt.tokens import RefreshToken
         
 class UserExistenceTest(TestCase):
     def test_user_exists(self):
@@ -43,16 +41,22 @@ class UserViewSetTestCase(TestCase):
         )
         self.client = APIClient()
 
-# test for viewing user list - user list only available to admin and staff users
+    def get_auth_header(self, user):
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+        return access_token
+
     def test_list_users_authenticated(self):
-        # Log in the user
-        self.client.login(username=self.admin_user.email, password="adminpassword")
+        access_token = self.get_auth_header(self.admin_user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+
         response = self.client.get('/api/users/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create_user_authenticated(self):
-        # Log in the user
-        self.client.login(username=self.user.email, password="testpassword")
+        access_token = self.get_auth_header(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+
         data = {
             "email": "newuser@example.com",
             "password": "newpassword"
@@ -61,24 +65,32 @@ class UserViewSetTestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_retrieve_user_authenticated(self):
-        # Log in the user
-        self.client.login(username=self.user.email, password="testpassword")
+        access_token = self.get_auth_header(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+
         response = self.client.get(f'/api/users/{self.user.id}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_user_authenticated(self):
-        # Log in the user
-        self.client.login(username=self.user.email, password="testpassword")
+        access_token = self.get_auth_header(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+
         data = {"email": "updated@example.com"}
         response = self.client.patch(f'/api/users/{self.user.id}/', data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.user.refresh_from_db()
         self.assertEqual(self.user.email, "updated@example.com")
-      
-# delete user test not working - 404 forbidden error, haven't solved
-    # def test_delete_user_authenticated(self):
-    #     # Log in the user
-    #     self.client.login(username=self.user.email, password="testpassword")
-    #     response = self.client.delete(f'/api/users/{self.user.id}/')
-    #     self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-    #     self.assertFalse(User.objects.filter(id=self.user.id).exists())
+    
+    
+    #Could not get this to work, seems to be Django delete permissions
+    #def test_delete_user_authenticated(self):
+     #   access_token = self.get_auth_header(self.user)
+       # self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+
+        #response = self.client.delete(f'/api/users/{self.user.id}/')
+        # # May want a more specific code for deleted user
+        #self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Verify the user has been deleted
+        #user_exists = User.objects.filter(id=self.user.id).exists()
+        #self.assertFalse(user_exists)
