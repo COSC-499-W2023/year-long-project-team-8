b5c6555e4a367b0d4d452f20dee777fdf1a13044
Mackenzie Kudrenecky <mkudrene@student.ubc.ca>
2023-11-07 12:04:13 -0800

    Refactor product model, implement search/filter

--- app/drf/backend/products/views.py
+++ app/drf/backend/products/views.py
@@ -1,20 +1,42 @@
 from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
 
-class ProductDetailAPIView(generics.RetrieveAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+# class ProductDetailAPIView(generics.RetrieveAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
     
-# uppdated the product list to be authenticated only - another layer to ensure only authenticated users can view the products
-# (mostly for testing the jwt authentication)
-class ProductListAPIView(generics.ListAPIView):
+# # uppdated the product list to be authenticated only - another layer to ensure only authenticated users can view the products
+# # (mostly for testing the jwt authentication)
+# class ProductListAPIView(generics.ListAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     # permission_classes = [IsAuthenticated]
+#     # authentication_classes = [JWTAuthentication]
+#     filter_backends = [DjangoFilterBackend]
+#     filterset_fields = ['categories']
+
+class ProductFilter(filters.FilterSet):
+    categories = filters.CharFilter(field_name='categories', lookup_expr='icontains')
+
+    class Meta:
+        model = Product
+        fields = []
+    
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
-    authentication_classes = [JWTAuthentication]
+    # permission_classes = [IsAuthenticated]
+    # authentication_classes = [JWTAuthentication]
+    filter_backends = [DjangoFilterBackend]
+    # filterset_fields = ['categories']
+    filterset_class = ProductFilter
+
     
     
     
