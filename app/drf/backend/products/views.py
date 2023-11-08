3206854eecc69d5c84d018e67f8866ecc9acc4fb
Mackenzie Kudrenecky <mkudrene@student.ubc.ca>
2023-11-07 18:14:28 -0800

    refactor for list of categories

--- app/drf/backend/products/views.py
+++ app/drf/backend/products/views.py
@@ -6,27 +6,25 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters import rest_framework as filters
+from django.db.models import Q
+import django_filters
 
-# class ProductDetailAPIView(generics.RetrieveAPIView):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
-    
-# # uppdated the product list to be authenticated only - another layer to ensure only authenticated users can view the products
-# # (mostly for testing the jwt authentication)
-# class ProductListAPIView(generics.ListAPIView):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
-#     # permission_classes = [IsAuthenticated]
-#     # authentication_classes = [JWTAuthentication]
-#     filter_backends = [DjangoFilterBackend]
-#     filterset_fields = ['categories']
-
-class ProductFilter(filters.FilterSet):
-    categories = filters.CharFilter(field_name='categories', lookup_expr='icontains')
+# Product filter to filter for CSV filter list
+class ProductFilter(django_filters.FilterSet):
+    categories = django_filters.CharFilter(method='filter_categories')
 
     class Meta:
         model = Product
         fields = []
+
+    def filter_categories(self, queryset, name, value):
+        categories = value.split(',')  # Split the comma-separated categories
+        q_objects = Q()
+
+        for category in categories:
+            q_objects |= Q(categories__icontains=category)
+
+        return queryset.filter(q_objects)
     
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
