5b49542c872d583a1efd1974e9bdd905defbd36c
Brandon Mack <brandonjmack@protonmail.com>
2023-11-23 01:08:00 -0800

    content into settings drawer, added manage profile button

--- app/drf/front-end/components/settingsPage/Settings.js
+++ app/drf/front-end/components/settingsPage/Settings.js
@@ -1,33 +1,46 @@
 import React from "react";
-import { View, Text, Image, StyleSheet, Button, TouchableOpacity, Dimensions} from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity, Dimensions } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-const windowHeight = Dimensions.get('window').height;
+// Get the width and height of the window for responsive styling
 const windowWidth = Dimensions.get('window').width;
 
 const SettingsPage = () => {
+  // Access the navigation object to navigate to other screens
   const navigation = useNavigation();
 
+  // Function to navigate to the Edit Profile page
   const goToEditProfile = () => {
-    // Navigate to the Edit Profile page
     navigation.navigate("EditProfile");
   };
 
   return (
     <View style={styles.container}>
-        <Text style={styles.headerText}>Settings</Text>
+      {/* Header Text */}
+      <Text style={styles.headerText}>Settings</Text>
+
+      {/* Manage Account Section */}
       <View style={styles.manageAccountContainer}>
+        {/* TouchableOpacity for pressing and navigating to Edit Profile */}
         <TouchableOpacity onPress={goToEditProfile}>
+          {/* Header for the Manage Account Section */}
           <Text style={styles.manageAccountHeader}> Manage Account </Text>
+          {/* Subheader for the Manage Account Section */}
           <Text style={styles.manageAccountSubHeader}> Update information and manage your account</Text>
+          {/* Right Arrow Image */}
           <Image source={require("../../assets/right-arrow.png")} style={styles.rightArrow}/>
         </TouchableOpacity>
       </View>
+      {/* Line to separate the Manage Account Section from the rest of the page */}
+      <View style={styles.lineStyle} />
     </View>
   );
 };
 
+// Styles for the component
 const styles = StyleSheet.create({
+  // light grey: #c1c1c1
+
   container: {
     flex: 1,
   },
@@ -41,9 +54,6 @@ const styles = StyleSheet.create({
     alignItems: "flex-start",
     marginLeft: 20,
     marginTop: 50,
-    borderColor: 'red',
-    borderWidth: 2
-
   },
   manageAccountHeader: {
     fontSize: 18,
@@ -57,14 +67,20 @@ const styles = StyleSheet.create({
     color: "#000000",
   },
   rightArrow: {
-  width: 15,
-  height: 15,
-  position: "absolute",
-  alignSelf: "flex-end",
-  top: "50%",
-  transform: [{ translateY: -7.5 }],
-},
-
+    width: 15,
+    height: 15,
+    alignSelf: 'flex-end',
+    position: 'absolute',
+    top: '50%',
+    left: windowWidth * 0.85,
+    transform: [{ translateY: -7.5 }],
+  },
+    lineStyle: {
+      borderWidth: 0.3,
+      borderColor: "#c1c1c1",
+      marginLeft: 25,
+      marginTop: 10,
+    },
 });
 
 export default SettingsPage;
