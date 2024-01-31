cdaf8b84e843790edf00b8f68cb81d9b23f00761
brandonjmack <brandonjmack@protonmail.com>
2024-01-30 18:18:35 -0800

    fixed issue where input fields would revert when not supposed to

--- app/drf/front-end/config/config.js
+++ app/drf/front-end/config/config.js
@@ -6,9 +6,12 @@
 //modify baseendpoint ip to test on expo, else use localhost
 
 //export const baseEndpoint = "http://192.168.1.67:8000/api";
-export const baseEndpoint = "http://142.231.161.57:8000/api";
+//export const baseEndpoint = "http://142.231.161.57:8000/api";
 //export const baseEndpoint = "http://localhost:8000/api";
 
 //export const baseEndpoint = "http://192.168.1.135:8000/api";
 //export const baseEndpoint = "http://142.231.166.135:8000/api";
 //export const baseEndpoint = "http://206.87.16.244:8000/api";
+
+// brandon
+export const baseEndpoint = "http://192.168.1.132:8000/api";
