ddb204c3cf139127315b0b070a1bc4686fdc0751
aroizmand <alexander.roizmand@gmail.com>
2024-01-31 22:41:33 -0800

    Fixed searchbar and category filtering

--- app/drf/front-end/config/config.js
+++ app/drf/front-end/config/config.js
@@ -5,8 +5,6 @@
 
 //modify baseendpoint ip to test on expo, else use localhost
 
-//export const baseEndpoint = "http://192.168.1.67:8000/api";
-
 //export const baseEndpoint = "http://142.231.161.57:8000/api";
 
 
