dc9fbc18a22bff33cbe1f0f1d58e3f8905f703f1
brandonjmack <brandonjmack@protonmail.com>
2024-02-04 20:03:26 -0800

    pushing to do weekly checkin
    fixing merge conflict with rebase

--- app/drf/front-end/config/config.js
+++ app/drf/front-end/config/config.js
@@ -18,3 +18,4 @@ export const baseEndpoint = "http://192.168.1.67:8000/api";
 //export const baseEndpoint = "http://192.168.1.135:8000/api";
 //export const baseEndpoint = "http://142.231.166.135:8000/api";
 //export const baseEndpoint = "http://206.87.16.244:8000/api";
+
