6a5c51587046370616c251347cea0e8fb7baff24
brandonjmack <brandonjmack@protonmail.com>
2024-02-11 16:01:11 -0800

    pushing to fix merge conflicts with rebase

--- app/drf/front-end/config/config.js
+++ app/drf/front-end/config/config.js
@@ -1,4 +1,3 @@
-
 // Replace with you ip or localhost (this will cover endpoints in JS files)
 // Add IP address to the allowed hosts and CORS_ORIGIN_WHITELIST in settings.py
 // run django server locally using python manage.py runserver 0.0.0.0:8000
@@ -10,6 +9,11 @@
 
 export const baseEndpoint = "http://192.168.1.67:8000/api";
 
+
 //export const baseEndpoint = "http://142.231.161.57:8000/api";
 
+//export const baseEndpoint = "http://localhost:8000/api";
 
+//export const baseEndpoint = "http://192.168.1.135:8000/api";
+//export const baseEndpoint = "http://142.231.166.135:8000/api";
+//export const baseEndpoint = "http://206.87.16.244:8000/api";
