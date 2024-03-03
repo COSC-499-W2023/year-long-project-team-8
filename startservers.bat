a82f49ffe473d7407d56e3279d4c2e37ed5052e4
Mackenzie Kudrenecky <mkudrene@student.ubc.ca>
2024-03-03 09:06:18 -0800

    add scripts to launch servers

---
+++ startservers.bat
@@ -0,0 +1,8 @@
+@echo off
+
+:: Navigate to the backend directory and start the backend server
+start cmd /k "cd app\drf && venv\Scripts\activate.bat && cd backend && python manage.py runserver 0.0.0.0:8000"
+
+:: Navigate to the front-end directory and start the Expo server
+start cmd /k "cd app\drf && venv\Scripts\activate && cd front-end\config && python update_config.py && cd .. && npx expo start"
+pause
