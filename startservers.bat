38d96ea9580322e587c5868c51da01371c9de0ce
Mackenzie Kudrenecky <mkudrene@student.ubc.ca>
2024-03-03 10:53:35 -0800

    edit: typo

--- startservers.bat
+++ startservers.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: Navigate to the backend directory and start the backend server
-start cmd /k "cd app\drf && venv\Scripts\activate.bat && cd backend && python manage.py runserver 0.0.0.0:8000"
+start cmd /k "cd app\drf && venv\Scripts\activate && cd backend && python manage.py runserver 0.0.0.0:8000"
 
 :: Navigate to the front-end directory and start the Expo server
 start cmd /k "cd app\drf && venv\Scripts\activate && cd front-end\config && python update_config.py && cd .. && npx expo start"
