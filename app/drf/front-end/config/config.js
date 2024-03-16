5384a710bbb389765e6a55bce8c68bf5357d6c93
aroizmand <alexander.roizmand@gmail.com>
2024-03-16 13:12:53 -0700

    added icon/splash screen + updated code version

--- app/drf/front-end/config/config.js
+++ app/drf/front-end/config/config.js
@@ -5,13 +5,13 @@
 
 //modify baseendpoint ip to test on expo, else use localhost
 
-export const baseEndpoint = "http://192.168.1.67:8000/api";
-//export const baseEndpoint = "http://192.168.1.67:8000/api";
+export const baseEndpoint = "http://142.231.160.222:8000/api";
+//export const baseEndpoint = "http://142.231.160.222:8000/api";
 
-//export const baseEndpoint = "http://192.168.1.67:8000/api";
+//export const baseEndpoint = "http://142.231.160.222:8000/api";
 
-//export const baseEndpoint = "http://192.168.1.67:8000/api";
+//export const baseEndpoint = "http://142.231.160.222:8000/api";
 
-//export const baseEndpoint = "http://192.168.1.67:8000/api";
-//export const baseEndpoint = "http://192.168.1.67:8000/api";
-//export const baseEndpoint = "http://192.168.1.67:8000/api";
+//export const baseEndpoint = "http://142.231.160.222:8000/api";
+//export const baseEndpoint = "http://142.231.160.222:8000/api";
+//export const baseEndpoint = "http://142.231.160.222:8000/api";
